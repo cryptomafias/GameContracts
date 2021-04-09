b9496ed6b8dcb2c89fa5d2edeacf3df6421f80f9
akashjha011 <51465755+akashjha011@users.noreply.github.com>
2021-04-09 20:48:35 +0530

    VRF and Gameplay contract update (#4)
    * Create RandomGenerator.sol
    
    Contract to generate random number using Chainlink VRF
    
    * Update CryptoMafiaCoin.sol
    
    Added gameplay functionalities
    
    * Added reward system and UX changes
    
    Added reward function and function to change address for interface. Getting playerID from their ERC721 now and some efficiency changes
    
    * Update CryptoMafiaCoin.sol
    
    Added check for if random number already exist and delete it when game is over
    
    * Change contains security updates
    
    Modifier for all address setting functions has been changed to only owner and double mapping removed.
    
    * Utility changes
    
    1. MafiaCoin contract doesn't pre-seed anymore
    2. Room stake has been hard coded to 80 for now and that amount is directly distributed to winners
    3. RequestRandomNumber now auto seeds the function with block hash of previous block
    4. GetRandomNumber now requires roomId parameter to return random number value
    5. Concurrent calls still get successfully stored in mapping according to request id and room number
    6. Create Room function has been removed
    7. Game over function has been removed
    
    * Completed requested changes

--- contracts/CryptoMafiaCoin.sol
+++ contracts/CryptoMafiaCoin.sol
@@ -4,12 +4,41 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+interface RandomInterface {
+  function getRandomNumber(uint256 userProvidedSeed) external returns (bytes32);
+  function randomNumber(uint256 roomId) external view returns(uint256);
+}
+
+interface AccountInterface{
+    function ownerOf(uint256 tokenId) external view returns (address);
+}
+
 contract CryptoMafiaCoin is ERC20, Ownable {
     address private _accountsHolder;
-
-    constructor(address accountsHolder, uint256 initialSupply) ERC20("Crypto Mafia Coin", "CMC") {
+    
+    address addressRandomGenerator;
+    
+    address contractAddress;
+    
+    RandomInterface RNG;
+    
+    AccountInterface AI;
+    
+    
+    constructor(address accountsHolder, uint256 initialSupply) ERC20("Crypto Mafia Coin Test", "CMC") {
         _mint(_msgSender(), initialSupply);
         _accountsHolder = accountsHolder;
+        contractAddress = address(this);
+        
+    }
+    
+    
+    function setAccountAddress(address accountAddress) public onlyOwner{
+        AI = AccountInterface(accountAddress);
+    }
+    
+    function setRandomAddress(address accountAddress) public onlyOwner{
+        RNG = RandomInterface(accountAddress);
     }
 
     modifier onlyAccountsHolder() {
@@ -24,4 +53,38 @@ contract CryptoMafiaCoin is ERC20, Ownable {
     function mint(address receiver, uint amount) onlyAccountsHolder public {
         _mint(receiver, amount);
     }
+    
+    
+    function RequestRandomNumberCMC() public returns(bytes32){
+        uint256 seed = uint256(blockhash(block.number-1));
+        return RNG.getRandomNumber(seed);
+        
+    }
+    
+    function GetRandomNumberCMC(uint256 roomId) public view returns(uint256) {
+        return RNG.randomNumber(roomId);
+    }
+    
+
+    function joinRoom() public payable
+    {   
+        transfer(contractAddress,10);
+    }
+    
+    
+    function distributeCoins(uint256[] memory playerId) public 
+    {
+        uint j;
+        uint len = playerId.length;
+        uint256 disbursalAmount;
+        
+        disbursalAmount = 80/len;
+        for (j = 0; j < len; j ++) {  
+            increaseAllowance(AI.ownerOf(playerId[j]),disbursalAmount);
+      }
+      
+     
+    }
+    
+    
 }
---
+++ contracts/RandomGenerator.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+
+contract RandomGenerator is VRFConsumerBase  {
+    
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    mapping (uint256=>uint256) randomResult; //room number to random number
+    
+    address private _owner;
+
+    mapping(bytes32=>uint256) roomRequest; //request ID to room number
+    
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * Constructor inherits VRFConsumerBase
+     * 
+     * Network: Mumbai Testnet
+     * Chainlink VRF Coordinator address: 0x8C7382F9D8f56b33781fE506E897a4F1e2d17255
+     * LINK token address:                0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * Key Hash: 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4
+     */
+    constructor() 
+        VRFConsumerBase(
+            0x8C7382F9D8f56b33781fE506E897a4F1e2d17255, // VRF Coordinator
+            0x326C977E6efc84E512bB9C30f76E30c160eD06FB  // LINK Token
+        ) public
+    {
+        keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
+        fee = 0.0001 * 10 ** 18; // 0.0001 LINK (varies by network)
+        address msgSender = msg.sender;
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+    
+    modifier onlyOwner() {
+        require(_owner == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+    
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+    
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+    
+    function getRandomNumber(uint256 userProvidedSeed) public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee, userProvidedSeed);
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+     
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult[roomRequest[requestId]] = randomness;
+}
+
+    function randomNumber(uint256 roomNumber) public view returns(uint256){
+        return randomResult[roomNumber];
+    }
+    
+    
+}
